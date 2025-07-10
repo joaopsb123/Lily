f53d4a03cf991eaa24cee9d1cdc630df15644a0a
joaopsb123 <joaopsb12223@gmail.com>
2025-07-10 19:31:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("loginBtn").addEventListener("click", () => {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=SEU_CLIENT_ID&redirect_uri=https%3A%2F%2Flily-production.up.railway.app%2Fauth%2Fcallback&response_type=code&scope=identify%20guilds";
+    window.location.href = "https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-production.up.railway.app%2F&scope=identify+email+guilds";
 });
