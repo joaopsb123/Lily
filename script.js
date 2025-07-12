86c4da3a59d07c88912a00dcc38835dd88d81f26
joaopsb123 <joaopsb12223@gmail.com>
2025-07-12 17:34:17 +0100

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("loginBtn").addEventListener("click", () => {
-    window.location.href = "https://discord.com/oauth2/authorize?client_id=1388158298305597483&response_type=code&redirect_uri=https%3A%2F%2Flily-red.vercel.app%2F&scope=identify+email+guilds";
-});
